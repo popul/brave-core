b1be3d26a3ab1937f134e96db876ea1f7a0cf141
Paul Musso <paul@musso.io>
2023-03-17 05:30:40 +0100

    Dummy resolution of dappy.d

---
+++ chromium_src/net/dns/host_resolver_manager.cc
@@ -0,0 +1,14 @@
+#define BRAVE_DAPPY_DOMAIN_CHECK                        \
+  if (GetHostname(job_key.host) == "dappy.d") {         \
+    HostCache::Entry entry = HostCache::Entry(OK, {     \
+      IPEndPoint(IPAddress(134,209,84,129), 0)}, {},    \
+      HostCache::Entry::SOURCE_DNS);                    \
+    request->set_results(                               \
+      entry.CopyWithDefaultPort(                        \
+        request->request_host().GetPort()));            \
+    request->set_error_info(OK, false);                 \
+    return OK;                                          \
+  }
+
+#include "src/net/dns/host_resolver_manager.cc"
+#undef BRAVE_DAPPY_DOMAIN_CHECK
